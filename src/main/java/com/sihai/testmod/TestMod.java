8ca125fa303a8a291e22c782f233dd4777c0b5a7
yu <1810564679@qq.com>
2025-03-05 13:37:57 +0800

    mod学习：方块添加

---
+++ src/main/java/com/sihai/testmod/TestMod.java
@@ -0,0 +1,43 @@
+package com.sihai.testmod;
+
+import com.mojang.logging.LogUtils;
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.SoundType;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+import org.slf4j.Logger;
+import org.spongepowered.asm.mixin.MixinEnvironment;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod(TestMod.MODID)
+public class TestMod
+{
+    // Define mod id in a common place for everything to reference
+    public static final String MODID = "testmod";
+    // Directly reference a slf4j logger
+    private static final Logger LOGGER = LogUtils.getLogger();
+    // Create a Deferred Register to hold Blocks which will all be registered under the "examplemod" namespace
+    public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, MODID);
+    // Create a Deferred Register to hold Items which will all be registered under the "examplemod" namespace
+    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, MODID);
+    // Create a Deferred Register to hold CreativeModeTabs which will all be registered under the "examplemod" namespace
+    public static final RegistryObject<Block> myblock = BLOCKS.register("myblock",()-> new Block(BlockBehaviour.Properties.of().strength(3.0f).sound(SoundType.CROP)));
+    public static final RegistryObject<Item> myblockitem = ITEMS.register("myblockitem",()-> new BlockItem(myblock.get(), new Item.Properties()));
+
+    public TestMod()
+    {
+        IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
+        ITEMS.register(bus);
+        BLOCKS.register(bus);
+
+
+    }
+}
